d4222bb52d65be64e04f0eae9fd20543dba4a0ca
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-28 12:53:30 -0500

    operations working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,14 @@
 var inputOne;
 var inputTwo;
 var answer;
+var operation;
+
 $(function(){
   console.log("Please be gentle.  It's my first time.");
   getOperators();
 
   $('#calculator').on('click', 'button', function(){
-    numberInput($(this).data('number'));
+    numberInput($(this).data());
   });
 });// end Document Ready
 
@@ -17,7 +19,7 @@ function createButtons(array){
   }
   for (var j = 0; j < array.length; j++) {
     console.log(array[j]);
-    $('#calculator').append("<button class=operations id='" + array[j].type +
+    $('#calculator').append("<button class=operations data-id='" + array[j].type +
     "'>" + array[j].operation + "</button>");
   }
 }
@@ -36,20 +38,38 @@ function getOperators (){
 }
 
 //function called when buttons are clicked
-function numberInput (number){
-  console.log(number);
-  if (number) {
-    $('#numInput').val($('#numInput').val() + number);
+function numberInput (data){
+  console.log(data.number, data.id);
+  if (data.number) {
+    $('#numInput').val($('#numInput').val() + data.number);
   } else if (typeof inputOne === "undefined"){
     inputOne = $('#numInput').val();
+    operation = data.id;
     $('#numInput').val('');
+  // } else if (typeof operation === "undefined") {
+  //   operation = data.id;
   } else {
     inputTwo = $('#numInput').val();
-    operations (inputOne, inputTwo);
+    operations (inputOne, inputTwo, operation);
   }
 }
 //statement for operations
-function operations (inputOne, inputTwo){
-  answer = parseInt(inputOne) + parseInt(inputTwo);
+function operations (inputOne, inputTwo, operation){
+  switch (operation){
+    case "add":
+      answer = parseInt(inputOne) + parseInt(inputTwo);
+      break;
+    case "subtract":
+      answer = parseInt(inputOne) - parseInt(inputTwo);
+      break;
+    case "multiply":
+      answer = parseInt(inputOne) * parseInt(inputTwo);
+      break;
+    case "divide":
+      answer = parseInt(inputOne) / parseInt(inputTwo);
+      break;
+    default:
+      console.log("Error finding answer");
+  }
   $("#numInput").val(answer);
 }
