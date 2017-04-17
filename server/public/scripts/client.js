8150e243760e7164c1fe9663bc1060f6992a38b6
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-04-17 12:29:01 -0500

    added scaffolding for memory functionality in both client and server side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -40,33 +40,72 @@ function createButtons(array){
   }
 }
 
+/*
+function that takes the button that is pressed and decides whether the button is
+a number or an operation.
+
+If the button is an operation, it evaluates whether the button is a special operation,
+such as equals or clear, and routes it on the proper path for that operation.
+
+If the button is a normal math operation, the function stores that operation and
+makes it possible for the second set of numbers to be pressed.
+*/
+
 function buttonInput(){
-  console.log($(this).data());
-  $('#calculator').data('lastInput', ($(this).data('id')));
+  var $el = $(this);
+  $('#calculator').data('lastInput', ($el.data('id')));
   var input;
-  if ($(this).data('number')){
-    input = ($(this).data('number'));
+  if ($el.data('number')){
+    input = ($el.data('number'));
     numberInput(input);
-  } else if ($(this).data('id') === 'equals'){
+  } else if ($el.data('id') === 'equals'){
     operationInput(input);
     $("#calculator").removeData('numberOne');
-    // console.log($("calculator").data('numberOne'));
-  } else if ($(this).data('id') === 'clear'){
+  } else if ($el.data('id') === 'clear'){
     clearInput();
     $("#calculator").removeData('numberOne');
     $("#calculator").removeData('operator');
     // console.log($("calculator").data());
-  } else if ($(this).data('id')){
-    input = ($(this).data('id'));
+  } else if ($el.data('id') === 'square'){
+    answer = $('#numInput').val() * $('#numInput').val();
+    clearInput();
+    $('#numInput').val(answer);
+    $("#calculator").removeData();
+    //$("#calculator").data('numberOne', answer);
+  } else if ($el.data('id') === 'sqRoot'){
+    answer = $('#numInput').val() / $('#numInput').val();
+    clearInput();
+    $('#numInput').val(answer);
+    $("#calculator").removeData();
+  } else if ($el.data('id') === 'plusMinus'){
+    //code that changes the value from positive to negative
+  } else if ($el.data('id') === 'mPlus'){
+    //put statement to update stored number
+  } else if ($el.data('id') === 'mMinus'){
+    //put statement to update stored number
+  } else if ($el.data('id') === 'mRecal'){
+    //retieves saved number from DB
+  } else if ($el.data('id') === 'mClear'){
+    //clears stored number from DB
+  }
+  else if ($el.data('id')){
+    input = ($el.data('id'));
     operationInput(input);
   }
 }
 
+//clears the input
 function clearInput(){
   $("#numInput").val('');
 }
 
-//function called when buttons are clicked
+/* function called when buttons are clicked.
+if there is information for the last input pressed, the calculator clears the
+input field and lastInput from data.
+
+if there is not any data for the last input pressed, then the calculator adds
+that number to the input field.
+*/
 function numberInput (input){
   console.log($("#calculator").data('lastInput'));
   // switch (data){
@@ -79,7 +118,13 @@ function numberInput (input){
   }
 }
 
-//function called when an operator is pushed
+/*
+function called when an operator is pushed
+if there is data for lastInput, then the the calculator information is sent to the
+operations function.
+Otherwise, the number that is displayed on the input is stored as numberOne, and
+the operator pressed is stored as operator.
+*/
 function operationInput(input){
   console.log($("#calculator").data('lastInput'));
   // console.log(data + " in operationInput path");
@@ -135,4 +180,34 @@ function getOperators (){
       createButtons(response);
     }
   }); // end ajax
+}
+
+function mPlus () {
+  $.ajax({
+    type: "UPDATE",
+    url: "memory/mPlus",
+    success: function(response){
+      //code to update Input field
+    }
+  });
+}
+
+function mRecal () {
+  $.ajax({
+    type: "GET",
+    url: "memory/mRecal",
+    success: function (response){
+      //code to display value on input field
+    }
+  });
+}
+
+function mDelete () {
+  $.ajax({
+    type: "DELETE",
+    url: "memory/mDelete",
+    success: function (response){
+      //code that shows cleared DOM
+    }
+  });
 }
---
+++ server/routes/memory.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+
+var config = {
+  database: "kevindahlberg", //DB name
+  host: 'localhost', //host for the DB
+  port: 5432, // port for the database
+  max: 10, // how many connections at one time
+  idleTimeoutMillis: 30000 // 30 seconds to try to connect
+};
+
+var pool = new pg.Pool(config); //allows us to have more than one connection at a time
+
+//scaffolding for GET statement.  Still needs to be set up to deal with mRecal
+router.get('/', function(req, res){
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      console.log("Error connecting to DB");
+      res.send(500);
+    } else {
+      console.log("connected");
+      client.query('SELECT * From "calculator";', function(queryError, result){
+        done();
+        if(queryError){
+          console.log("Error making query.");
+          res.sendStatus(500);
+        } else {
+          console.log("calculator sent");
+          res.send(result.rows);
+        }
+      });
+    }
+  });
+}); // end function
+
+//post function to add list item to database
+router.post('/newItem', function(req, res){
+  console.log(req.body);
+  var description = req.body.description;
+  var completed = req.body.completed;
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      console.log("Error connecting to DB");
+      res.send(500);
+    } else {
+      console.log("connected");
+      client.query("INSERT INTO list (description, completed) VALUES ($1, $2);", [description, completed], function(queryError, result){
+        done();
+        if(queryError){
+          console.log("Error making query.");
+          res.send(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    }
+  });
+});// end post function
+
+//DELETE function to delete from search database
+router.delete('/delete/:listId', function(req, res){
+  console.log("in the delete function: ", req.params);
+  var id = req.params.listId;
+pool.connect(function(errorConnectingToDatabase, client, done){
+  if(errorConnectingToDatabase) {
+    console.log("Error connecting to DB");
+    res.send(500);
+  } else {
+    console.log("connected");
+    client.query('DELETE FROM list WHERE "id" = $1', [id], function(queryError, result){
+      done();
+      if(queryError){
+        console.log("Error making query.");
+        res.send(500);
+      } else {
+        res.sendStatus(201);
+      }
+    });
+  }
+});
+});// end delete function
+
+//put function to update completed on list from false to true
+router.put('/completed', function(req, res){
+  console.log(req.body);
+  var completed = req.body.completed;
+  var id = req.body.id;
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      console.log("Error connecting to DB");
+      res.send(500);
+    } else {
+      console.log("connected");
+      client.query('UPDATE "list" SET "completed" = $2 WHERE "id" = $1', [id, completed], function(queryError, result){
+        done();
+        if(queryError){
+          console.log("Error making query.");
+          res.send(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    }
+  });
+}); //end PUT function
